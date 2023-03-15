1191f5597b4601fcf3719e81c70f90053ef0374b
Yago Hilario <gityago@gmail.com>
2023-03-14 23:40:20 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import logoImage from './assets/devmemory_logo.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.Info}>
+        <div className={styles.logoLink}>
+            <img src={logoImage} alt="" width="200" />
+        </div>
+
+        <div className={styles.infoArea}>
+          ...
+        </div>
+
+        <button>Reiniciar</button>
+      </div>
+
+      <div className={styles.gridArea}>
+        ...
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
